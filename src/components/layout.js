d21068839128f7d8ca01f2023dff5f79369a6736
TechJohnson <steven@techjohnson.com>
2021-01-20 12:22:42 -0500

    Update copyright information

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,9 +22,7 @@ const Layout = ({ children }) => {
       <div>
         <main>{children}</main>
         <footer style={{marginTop: `2rem`,}}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          Copyright © {new Date().getFullYear()} Replacement Windows Online
         </footer>
       </div>
     </>
