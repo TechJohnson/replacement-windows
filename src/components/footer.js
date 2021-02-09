4e1c13c103c450640b5326a82a40a52bdd7a19a8
Steven Johnson <steven@techjohnson.com>
2021-02-09 15:37:58 -0500

    Update contact form

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Responsive from "react-responsive"
 import Img from "gatsby-image"
-import ContactForm from "../components/contactForm"
 
 
 
@@ -29,8 +28,30 @@ const Footer = () => {
 
                 <Desktop>
                     {/* Contact Form */}
-                    <div id="contact" style={{padding: '0 0 100px 0'}}>
-                        <ContactForm />
+                    <div style={{
+                        paddingBottom: '100px',
+                    }}>
+                        <h2 style={{textAlign: 'center'}}>Let's get in touch</h2>
+                        <p style={{textAlign: 'center'}}>Shoot us a quick email to gather some more information</p>
+
+                        <form name="contactDesktop" method="POST" data-netlify="true" netlify>
+                            <div className="inputArea">
+                                <div className="inputs">
+                                    <input type="text" name="name" id="name" placeholder="name"/>
+                                </div>
+                                <div className="inputs">
+                                    <input type="email" name="email" id="email" placeholder="email"/>
+                                </div>
+                                <div className="inputs">
+                                    <input type="tel" name="number" id="number" placeholder="phone"/>
+                                </div>
+                            </div>
+                            <div className="messsage">
+                                <textarea name="message" placeholder="Which window would you like more information about?"></textarea>
+                            </div>
+                            <div data-netlify-recaptcha="true"></div>
+                            <button class="submit-btn" type="submit">Send Message</button>
+                        </form>
                     </div>
 
                     {/* Footer Information */}
