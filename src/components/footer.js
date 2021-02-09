11096672cb5adb0a8c43658c1522780cbc4e2b55
Steven Johnson <steven@techjohnson.com>
2021-02-09 15:45:33 -0500

    Update homepage contact form

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,33 +27,6 @@ const Footer = () => {
             <div className="container">
 
                 <Desktop>
-                    {/* Contact Form */}
-                    <div style={{
-                        paddingBottom: '100px',
-                    }}>
-                        <h2 style={{textAlign: 'center'}}>Let's get in touch</h2>
-                        <p style={{textAlign: 'center'}}>Shoot us a quick email to gather some more information</p>
-
-                        <form name="contactDesktop" method="POST" data-netlify="true" netlify>
-                            <div className="inputArea">
-                                <div className="inputs">
-                                    <input type="text" name="name" id="name" placeholder="name"/>
-                                </div>
-                                <div className="inputs">
-                                    <input type="email" name="email" id="email" placeholder="email"/>
-                                </div>
-                                <div className="inputs">
-                                    <input type="tel" name="number" id="number" placeholder="phone"/>
-                                </div>
-                            </div>
-                            <div className="messsage">
-                                <textarea name="message" placeholder="Which window would you like more information about?"></textarea>
-                            </div>
-                            <div data-netlify-recaptcha="true"></div>
-                            <button class="submit-btn" type="submit">Send Message</button>
-                        </form>
-                    </div>
-
                     {/* Footer Information */}
                     <div style={{
                         display: 'flex',
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,33 @@ const Homepage = () => (
 
     <WindowOptions />
 
+    <div className="container">
+    <div style={{
+          paddingBottom: '100px',
+      }}>
+          <h2 style={{textAlign: 'center'}}>Let's get in touch</h2>
+          <p style={{textAlign: 'center'}}>Shoot us a quick email to gather some more information</p>
+
+          <form name="desktop-homepage" method="POST" data-netlify="true" netlify>
+              <div className="inputArea">
+                  <div className="inputs">
+                      <input type="text" name="name" id="name" placeholder="name"/>
+                  </div>
+                  <div className="inputs">
+                      <input type="email" name="email" id="email" placeholder="email"/>
+                  </div>
+                  <div className="inputs">
+                      <input type="tel" name="number" id="number" placeholder="phone"/>
+                  </div>
+              </div>
+              <div className="messsage">
+                  <textarea name="message" placeholder="Which window would you like more information about?"></textarea>
+              </div>
+              <button class="submit-btn" type="submit">Send Message</button>
+          </form>
+      </div>
+    </div>
+
   </Layout>
 )
 
