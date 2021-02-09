f4c6d965fc1183811b200a2056f73e19e16239e5
Steven Johnson <steven@techjohnson.com>
2021-02-09 17:33:50 -0500

    Form ID

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const Homepage = () => (
           <h2 style={{textAlign: 'center'}}>Let's get in touch</h2>
           <p style={{textAlign: 'center'}}>Shoot us a quick email to gather some more information</p>
 
-          <form name="Homepage" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form name="Homepage" id="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
             <div className="inputArea">
               <div className="inputs">
                 <input type="text" name="name" id="name" placeholder="name"/>
