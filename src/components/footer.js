01e199ce133809ef6cd405133360ab0864c4d301
Steven Johnson <steven@techjohnson.com>
2021-02-09 19:26:20 -0500

    Functioning on homepage

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,37 +27,6 @@ const Footer = () => {
             <div className="container">
 
                 <Desktop>
-
-                <div className="formBackground">
-      <div className="container">
-        <div style={{
-        paddingBottom: '100px',
-        }}>
-          <h2 style={{textAlign: 'center'}}>Let's get in touch</h2>
-          <p style={{textAlign: 'center'}}>Shoot us a quick email to gather some more information</p>
-
-          <form name="Homepage" id="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-          <input type="hidden" name="Homepage" value="Homepage" />
-            <div className="inputArea">
-              <div className="inputs">
-                <input type="text" name="name" id="name" placeholder="name"/>
-              </div>
-              <div className="inputs">
-                <input type="email" name="email" id="email" placeholder="email"/>
-              </div>
-              <div className="inputs">
-                <input type="tel" name="number" id="number" placeholder="phone"/>
-              </div>
-            </div>
-            <div className="messsage">
-              <textarea name="message" placeholder="Which window would you like more information about?"></textarea>
-            </div>
-              <button className="submit-btn" type="submit">Send Message</button>
-          </form>
-        </div>
-      </div>
-    </div>
-    
                     {/* Footer Information */}
                     <div style={{
                         display: 'flex',
