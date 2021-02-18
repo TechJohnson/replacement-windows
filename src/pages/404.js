afc7fed68bc23f8f1370159a5416d803d8e87786
TechJohnson <steven@techjohnson.com>
2021-02-17 20:59:49 -0500

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,53 @@
 import React from "react"
-
-import Layout from "../components/layout"
+import Button from "../components/button"
 import SEO from "../components/seo"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      lost: file(relativePath: { eq: "404-lost.png" }) {
+        childImageSharp {
+          fixed(width: 300) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+
+  <div style={{
+    background: 'rgb(247, 250, 253)',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center'
+  }}>
+
+  <SEO title="Uh oh! It looks like we can't find that page." />
+
+
+    <div className="container">
+
+      <div>
+        <Img fixed={data.lost.childImageSharp.fixed} />
+        <h1 style={{fontWeight: '600', paddingBottom: '25px', fontSize: '55px'}}>Uh oh! Are you lost?</h1>
+        <p style={{marginBottom: '25px'}}>The page you requested could not be found. We suggest going back to the homepage.</p>
+        <Link to={'/'}>
+          <Button buttonText="Go Home"/>
+        </Link>
+      </div>
+
+    </div>
+
+  </div>
+
+  )
+  
+}
 
 export default NotFoundPage
