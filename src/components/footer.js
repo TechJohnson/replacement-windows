70ce04149f726b16cb486669348dfb34eb7c4932
TechJohnson <steven@techjohnson.com>
2021-06-17 22:56:04 -0400

    Update footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -46,7 +46,7 @@ const Footer = () => {
                             flexDirection: 'column',
                             width: '25%'
                         }}>
-                            <Link to={'/#procedure'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Our Procedure</Link>
+                            <Link to={'/our-procedure'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Our Procedure</Link>
                             <Link to={'/affordability'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Affordability</Link>
                             <Link to={'/#contact'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Contact Us</Link>
                             <Link to={'/faq'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>FAQ</Link>
@@ -88,7 +88,7 @@ const Footer = () => {
                             padding: '50px 0px',
                             width: '100%'
                         }}>
-                            <Link to={'/#procedure'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Our Procedure</Link>
+                            <Link to={'/our-procedure'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Our Procedure</Link>
                             <Link to={'/affordability'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Affordability</Link>
                             <Link to={'/#contact'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>Contact Us</Link>
                             <Link to={'/faq'} style={{fontSize: '1.125rem', color: '#71808E', textDecoration: 'none', marginBottom: '20px'}}>FAQ</Link>
